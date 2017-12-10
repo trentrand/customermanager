a2d324770f98540f19f8607689b8a81c90e311f4
Trent Rand <contact@trentrand.com>
2017-12-10 14:09:08 -0700

    Remove express server

--- server/routes/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET api listing. */
-router.get('/', (req, res) => {
-  res.send('Api listening on port 3000');
-});
-
-module.exports = router;
